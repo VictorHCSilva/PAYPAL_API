a1cd084ffa25af718a94b35e519aaf5fc7305ca3
VICTOR HUGO COSTA DA SILVA <victor.hcsilva13@senacsp.edu.br>
2022-04-05 11:42:05 -0300

    Introdução referente a integração do PAYPAL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
-export const PAYPAL_API_CLIENT = 'ASK4oFgU6kYHssrzLiFbNndaFU7kqwpkznjkfPFDuUSYwGnkAdXuE4ZiDZx8GQnk17TEZ_8M7G2aR3ba'
-export const PAYPAL_API_SECRET = 'EOlSSa3b0T-k9dK4uQODPu-PncutypavhbhbsxxDTOd-StpexdButmI6de1i1qWErH5aCqzvE4fmIdFp'
-export const PAYPAL_API = 'https://api-m.sandbox.paypal.com/v1/oauth2/token'
+export const PAYPAL_API_CLIENT = 
+'ASK4oFgU6kYHssrzLiFbNndaFU7kqwpkznjkfPFDuUSYwGnkAdXuE4ZiDZx8GQnk17TEZ_8M7G2aR3ba'
+export const PAYPAL_API_SECRET = 
+'EOlSSa3b0T-k9dK4uQODPu-PncutypavhbhbsxxDTOd-StpexdButmI6de1i1qWErH5aCqzvE4fmIdFp'
+export const PAYPAL_API = 
+'https://api-m.sandbox.paypal.com'
 
 export const PORT = 4000
--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,11 +1,45 @@
-export const createorder = (req, res) =>{
-    res.send('creating order')
-}
+//import axios from "axios"
+import { PAYPAL_API, PAYPAL_API_CLIENT, PAYPAL_API_SECRET } from "../config";
+
+export const createorder = async (req, res) =>{
+
+    const order = {
+        intent: "CAPTURE",
+        purchase_units: [
+            {
+                amount: {
+                    currency_code: "USD",
+                    value: "25,00"
+                },
+                description: "aplication installer",
+            },
+        ],
+        application_context:{
+            brand_name:"Default Application",
+            landing_page:"LOGIN",
+            user_action: "PAY_NOW",
+            return_url:"http://localhost:4000/capture-order",
+            cancel_url:"http://localhost:4000/cancel-order",
+        },
+    };
+
+    const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders`, order, {
+        auth:{
+            username: PAYPAL_API_CLIENT,
+            password: PAYPAL_API_SECRET,
+        }
+
+    });
+
+    console.log(response)
+
+    res.send('creating order');
+};
 
 export const captureOrder = (req, res) =>{
-    res.send('capituring order')
-}
+    res.send('capituring order');
+};
 
 export const cancelOrder = (req, res) =>{
-    res.send('cancel an order')
-}
+    res.send('cancel an order');
+};
